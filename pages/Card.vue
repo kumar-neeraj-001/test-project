014b3595523ee19e74e12929cb6d128af607ddcd
kumar-neeraj-001 <98804538+kumar-neeraj-001@users.noreply.github.com>
2022-04-20 13:03:19 +0530

    new

---
+++ pages/Card.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div
+      class="max-w-sm rounded-xl overflow-hidden shadow hover:shadow-lg"
+      :class="changeColor"
+    >
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p class="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#photography</span
+        >
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#travel</span
+        >
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#winter</span
+        >
+      </div>
+    </div>
+    <ul>
+      <li v-for="{ item, index } in ['cd', 'cnds', 'ckjndsk']" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+    <h1 class="text-2xl">
+      {{ windowWidth }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  data() {
+    return {
+      windowWidth: window.innerWidth,
+    };
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener("resize", this.onResize);
+    });
+  },
+
+  computed: {
+    changeColor() {
+      if (this.windowWidth > 600) {
+        return "bg-green-400";
+      }
+      return "bg-yellow-400";
+    },
+  },
+
+  methods: {
+    onResize() {
+      this.windowWidth = window.innerWidth;
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,15 @@
 <template>
   <div>
-    <!-- <router-link to="/blog">Blog</router-link> -->
-    <Date v-model="date" />
-
-    <!-- <date-picker
-      v-model="date"
-      range
-      :disabled="false"
-      placeholder="Neeraj Date Picker"
-    >
-      <template #icon-calendar> ICON </template>
-      <template #icon-clear> CLEAR </template>
-      <template v-slot:header="{ emit }">
-        <div class="w-full">
-          <button class="mx-btn mx-btn-text" @click="selectNextThreeDay(emit)">
-            Next Three Day
-          </button>
-        </div>
-      </template>
-    </date-picker> -->
-    <button @click="printTime">click</button>
+    <Card />
   </div>
 </template>
 
 <script>
 import Vue from "vue";
-import Date from "./Date.vue";
+import Card from "./Card.vue";
 
 export default Vue.extend({
   name: "Index",
-  components: { Date },
-  data() {
-    return {
-      myName: "",
-      date: null,
-    };
-  },
-  methods: {
-    printTime() {
-      console.log(this.myName);
-      console.log(this.date);
-    },
-    neerajdis(date) {
-      return date > new Date();
-    },
-  },
+  components: { Card },
 });
 </script>
